98131e0759a41d9ead0978316e6c4b6d750cb729
tony <android.rokitnicki@gmail.com>
2015-01-02 00:58:54 +0100

    config.xml doesnt matter
    AndroidManifest is used by build

--- platforms/android/ant-build/classes.dex.d
+++ platforms/android/ant-build/classes.dex.d
@@ -6,4 +6,11 @@ D:\GitHub\RandomGenerator-PG\platforms\android\ant-build\classes\com\phonegap\he
 D:\GitHub\RandomGenerator-PG\platforms\android\ant-build\classes\com\phonegap\helloworld\R$string.class \
 D:\GitHub\RandomGenerator-PG\platforms\android\ant-build\classes\com\phonegap\helloworld\R$xml.class \
 D:\GitHub\RandomGenerator-PG\platforms\android\ant-build\classes\com\phonegap\helloworld\R.class \
+D:\GitHub\RandomGenerator-PG\platforms\android\ant-build\classes\com\random\generator\pg\BuildConfig.class \
+D:\GitHub\RandomGenerator-PG\platforms\android\ant-build\classes\com\random\generator\pg\CordovaApp.class \
+D:\GitHub\RandomGenerator-PG\platforms\android\ant-build\classes\com\random\generator\pg\R$attr.class \
+D:\GitHub\RandomGenerator-PG\platforms\android\ant-build\classes\com\random\generator\pg\R$drawable.class \
+D:\GitHub\RandomGenerator-PG\platforms\android\ant-build\classes\com\random\generator\pg\R$string.class \
+D:\GitHub\RandomGenerator-PG\platforms\android\ant-build\classes\com\random\generator\pg\R$xml.class \
+D:\GitHub\RandomGenerator-PG\platforms\android\ant-build\classes\com\random\generator\pg\R.class \
 D:\GitHub\RandomGenerator-PG\platforms\android\ant-build\dexedLibs\classes-f9ef0c5fe0b8209679bcff03441f09a1.jar \
